41716226a45e5a9b22d564aece1758526027205d
Rafael Neves <rafaelneves652@gmail.com>
2026-08-18 14:42:23 -0300

    feat/curinthiaehomelhor

--- api/app/api/photo.js
+++ api/app/api/photo.js
@@ -11,7 +11,7 @@ const userCanDelete = user => photo => photo.userId == user.id;
 const defaultExtension = '.jpg';
 
 api.list = async (req, res) => {
-    console.log('TA TUDO ERRADO, faz direito!!!!!!!');
+    console.log('Vai Corinthians');
     
     const { userName } = req.params;
     const { page } = req.query;
